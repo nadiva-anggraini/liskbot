459062c199de9d8c019c439426b6cf510f1639b8
Nadiva Anggraini <119149860+nadiva-anggraini@users.noreply.github.com>
2025-01-22 04:32:39 +0700

    Update register.js

--- register.js
+++ register.js
@@ -187,40 +187,40 @@ async function fetchAirdropUser(wallet) {
       switch (userData.verifiedStatus) {
         case "IS_FULLY_VERIFIED":
           console.log(kleur.green("User is fully verified. Continuing process..."));
-	  await loading(`Checkin Task...`, 2000);
-	  const Checkin = await performCheckin(wallet);
-	  await loading(`Follow Task...`, 2000);
-          await followTask(wallet);
-	  await loading(`Hold ETH Task...`, 2000);
-	  await holdETH(wallet);
-	  await loading(`Hold LSK Task...`, 2000);
-	  await holdLSK(wallet);
-	  await loading(`Hold USDC Task...`, 2000);
-	  await holdUSDC(wallet);
-	  await loading(`Hold USDT Task...`, 2000);
-	  await holdUSDT(wallet);
-	  await loading(`Checking User data...`, 2000);
-	  await checkAccount(wallet);
+		  await loading(`Checkin Task...`, 2000);
+		  const fistCheckin = await performCheckin(wallet);
+	      await loading(`Follow Task...`, 2000);
+          const followMissin = await followTask(wallet);
+	      await loading(`Hold ETH Task...`, 2000);
+	      const ethMission = await holdETH(wallet);
+	      await loading(`Hold LSK Task...`, 2000);
+	      const lskMission = await holdLSK(wallet);
+	      await loading(`Hold USDC Task...`, 2000);
+	      const usdcMission = await holdUSDC(wallet);
+	      await loading(`Hold USDT Task...`, 2000);
+	      const usdtMission = await holdUSDT(wallet);
+	      await loading(`Checking User data...`, 2000);
+	      const dataAccount = await checkAccount(wallet);
           console.log('All Done');
           break;
 
         case "IS_GUILD_VERIFIED":
           await loading(`User is guild verified, Register user to Airdrop`, 6000);
           await addAirdropUser(wallet, referralCode);
-	  await loading(`Checkin Task...`, 2000);
-	  await performCheckin(wallet);
-	  await loading(`Follow Task...`, 2000);
-          await followTask(wallet);
-	  await loading(`Hold ETH Task...`, 2000);
-	  await holdETH(wallet);
-	  await loading(`Hold LSK Task...`, 2000);
-	  await holdLSK(wallet);
-	  await loading(`Hold USDC Task...`, 2000);
-	  await holdUSDC(wallet);
-	  await loading(`Hold USDT Task...`, 2000);
-	  await holdUSDT(wallet);
-	  await loading(`Checking User data...`, 2000);
-	  await checkAccount(wallet);
+		  await loading(`Checkin Task...`, 2000);
+		  const fistCheckin = await performCheckin(wallet);
+	      await loading(`Follow Task...`, 2000);
+          const followMissin = await followTask(wallet);
+	      await loading(`Hold ETH Task...`, 2000);
+	      const ethMission = await holdETH(wallet);
+	      await loading(`Hold LSK Task...`, 2000);
+	      const lskMission = await holdLSK(wallet);
+	      await loading(`Hold USDC Task...`, 2000);
+	      const usdcMission = await holdUSDC(wallet);
+	      await loading(`Hold USDT Task...`, 2000);
+	      const usdtMission = await holdUSDT(wallet);
+	      await loading(`Checking User data...`, 2000);
+	      const dataAccount = await checkAccount(wallet);
           console.log('All Done');
           break;
 
@@ -309,11 +309,22 @@ async function performCheckin(wallet) {
       },
     });
 
-    console.log(kleur.green(`Check-in successful for ${wallet.address}`));
-    return response.data;
+    if (response.data.errors) {
+      const errorMessage = response.data.errors[0]?.message || "Unknown error";
+      console.error(kleur.red(`Checkin Task failed for ${wallet.address}: ${errorMessage}`));
+      return {
+        success: false,
+        message: errorMessage,
+      };
+    }
+    console.log(kleur.green(`Checkin Task successful for ${wallet.address}`));
+    return {
+      success: true,
+      data: response.data,
+    };
   } catch (error) {
     console.error(
-      kleur.yellow(`Check-in failed for ${wallet.address}: `),
+      kleur.yellow(`Checkin Task failed for ${wallet.address}: `),
       error.message
     );
     throw error;
@@ -343,8 +354,19 @@ async function followTask(wallet) {
       },
     });
 
+    if (response.data.errors) {
+      const errorMessage = response.data.errors[0]?.message || "Unknown error";
+      console.error(kleur.red(`Follow Task failed for ${wallet.address}: ${errorMessage}`));
+      return {
+        success: false,
+        message: errorMessage,
+      };
+    }
     console.log(kleur.green(`Follow Task successful for ${wallet.address}`));
-    return response.data;
+    return {
+      success: true,
+      data: response.data,
+    };
   } catch (error) {
     console.error(
       kleur.yellow(`Follow Task failed for ${wallet.address}: `),
